255675321ba4d6edebeac1d8c8071fa466c854c1
igorbaloshin <iebaloshin@gmail.com>
2023-02-20 13:06:55 +0300

    пример 12 2

---
+++ Example011_2_array_sel/Program.cs
@@ -0,0 +1,46 @@
+﻿// Сортировка массива
+
+Console.Clear();
+
+int[] arr = {1, 5, 6, 7, 8, 9, 3, 5,};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        //int minPosition = i;
+        int maxPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            //if(array[j] < array[minPosition])
+            if(array[j] > array[maxPosition])
+            //minPosition = j;
+            maxPosition = j;
+
+        }
+
+        int temporary = array[i];
+        //array[i] = array[minPosition];
+        array[i] = array[maxPosition];
+        //array[minPosition] = temporary;
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
+
