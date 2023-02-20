17e2888a9fbc0c7464cacaa2c0a64b1f772993d9
igorbaloshin <iebaloshin@gmail.com>
2023-02-20 12:39:12 +0300

    дополнили примеры 11

---
+++ Example011_ Cycle_For/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+// Цикл FOR
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for(int i = 1; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+
+// }
+
+// string res = Method4(10, "Ура, ");
+// Console.WriteLine(res);
+//-------------------------------------------------
+// Печать таблицы умножения
+
+// for(int i = 2; i <= 10; i++)
+// {
+//     for(int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} X {j} = {i * j}");
+
+//     }
+//     Console.WriteLine();
+// }
+//--------------------------------------------------
+//
---
+++ Example011_2_array/Program.cs
@@ -0,0 +1,19 @@
+﻿// Сортировка массива
+
+Console.Clear();
+
+int[] arr = {1, 5, 6, 7, 8, 9, 3, 5,};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    
+}
+
+PrintArray(arr);
